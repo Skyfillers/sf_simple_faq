1778df66e3168b0504dcf50693884a402027ce9d
daniel <D.Meyer@skyfillers.com>
2015-03-04 12:43:23 +0100

    Added flexform to plugin

--- ext_tables.php
+++ ext_tables.php
@@ -9,6 +9,13 @@ if (!defined('TYPO3_MODE')) {
 	'Simple FAQ'
 );
 
+/* Add Flexform */
+$extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY);
+$pluginSignature = strtolower($extensionName) . '_pifaq';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/Flexform_plugin.xml');
+
+/* Static TypoScript */
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Simple FAQ');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_sfsimplefaq_domain_model_category', 'EXT:sf_simple_faq/Resources/Private/Language/locallang_csh_tx_sfsimplefaq_domain_model_category.xlf');
