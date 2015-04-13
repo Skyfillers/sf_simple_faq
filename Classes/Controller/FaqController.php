4aad2858df0e07854cfea41895bc556281e48137
Alexander Schnoor <a.schnoor@skyfillers.com>
2015-04-13 11:19:06 +0200

    A ViewHelper generates the the argument "selectedCategories". Changed "category" to "categories" where it was possible to make the code more logical. Delete unnecessary files and cleanup of the code.

--- Classes/Controller/FaqController.php
+++ Classes/Controller/FaqController.php
@@ -1,39 +1,25 @@
 <?php
 namespace SKYFILLERS\SfSimpleFaq\Controller;
 
-
-/***************************************************************
- *
- *  Copyright notice
- *
- *  (c) 2015 Daniel Meyer <d.meyer@skyfillers.com>
- *
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+	/*                                                                        *
+	 * This script is backported from the TYPO3 Flow package "TYPO3.Fluid".   *
+	 *                                                                        *
+	 * It is free software; you can redistribute it and/or modify it under    *
+	 * the terms of the GNU Lesser General Public License, either version 3   *
+	 *  of the License, or (at your option) any later version.                *
+	 *                                                                        *
+	 * The TYPO3 project - inspiring people to share!                         *
+	 *                                                                        */
 
 /**
  * FaqController
+ *
+ * @author Daniel Meyer, Alexander Schnoor
  */
 class FaqController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController {
 
 	/**
-	 * faqRepository
+	 * The FaqRepository
 	 *
 	 * @var \SKYFILLERS\SfSimpleFaq\Domain\Repository\FaqRepository
 	 * @inject
@@ -41,69 +27,62 @@ class FaqController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController {
 	protected $faqRepository = NULL;
 
 	/**
-	 * categoryRepository
+	 * The categoryRepository
 	 *
 	 * @var \SKYFILLERS\SfSimpleFaq\Domain\Repository\CategoryRepository
 	 * @inject
 	 */
 	protected $categoryRepository = NULL;
 
-    /**
-     * filterFagHelper
-     *
-     * @var \SKYFILLERS\SfSimpleFaq\Helper\FilterFaqHelper
-     * @inject
-     */
-    protected $filterFagHelper = NULL;
-
 	/**
 	 * Create a demand object with the given settings
-     *
-	 * @param array $settings
-	 * @param string $category
-	 * @param string $searchText
+	 *
+	 * @param array $settings The settings
+	 * @param string $searchText The text, which the FAQs are filtered by
+	 * @param string $categories The categories, which the FAQs are filtered by
+	 *
 	 * @return \SKYFILLERS\SfSimpleFaq\Domain\Model\Dto\FaqDemand
 	 */
-	public function createDemandObjectFromSettings($settings, $searchText, $category = '0') {
-		if ($category === '0') {
-			$category = $settings['category'];
+	public function createDemandObjectFromSettings(array $settings, $searchText, $categories = '0') {
+		if ($categories === '0') {
+			$categories = $settings['category'];
 		}
 
-		/** @var \SKYFILLERS\SfSimpleFaq\Domain\Model\Dto\FaqDemand $demand */
+		/**
+		 * Object of the type FaqDemand
+		 *
+		 * @var \SKYFILLERS\SfSimpleFaq\Domain\Model\Dto\FaqDemand $demand
+		 */
 		$demand = $this->objectManager->get('SKYFILLERS\\SfSimpleFaq\\Domain\\Model\\Dto\\FaqDemand');
 		$demand->setSearchText($searchText);
-		$demand->setCategory($category);
+		$demand->setCategories($categories);
 
 		return $demand;
 	}
 
 	/**
-	 * action list
+	 * The action list
 	 *
-	 * @param string $selectedCategories
-	 * @param string $actualCategory
-     * @param string $searchText
+	 * @param string $selectedCategories The categories to filter the FAQs
+	 * @param string $searchText The searchText to filter the FAQs
+	 *
 	 * @return void
 	 */
-	public function listAction($selectedCategories = '0', $actualCategory = '0', $searchText = '') {
+	public function listAction($selectedCategories = '0', $searchText = '') {
 
-        $categories = $this->categoryRepository->findAll();
+		$categories = $this->categoryRepository->findAll();
 
-        /** @var \SKYFILLERS\SfSimpleFaq\Helper\FilterFaqHelper $filterFagHelper */
-        $this->filterFagHelper = $this->objectManager->get('SKYFILLERS\\SfSimpleFaq\\Helper\\FilterFaqHelper');
-        $selectedCategories = $this->filterFagHelper->buildFilterArray($selectedCategories, $actualCategory);
-
-        $demand = $this->createDemandObjectFromSettings($this->settings, $searchText, $selectedCategories);
+		$demand = $this->createDemandObjectFromSettings($this->settings, $searchText, $selectedCategories);
 		$faqs = $this->faqRepository->findDemanded($demand);
 
-        $assignArray = array(
-            'faqs' => $faqs,
-            'categories' => $categories,
-            'selectedCategories' => $selectedCategories,
-            'searchText' => $searchText,
-            'actualCategory' => $actualCategory,
-        );
-        $this->view->assignMultiple($assignArray);
+		$assignArray = array(
+			'faqs' => $faqs,
+			'categories' => $categories,
+			'selectedCategories' => $selectedCategories,
+			'searchText' => $searchText,
+		);
+
+		$this->view->assignMultiple($assignArray);
 	}
 
 }
--- Classes/ViewHelpers/CategoryActiveViewHelper.php
+++ Classes/ViewHelpers/CategoryActiveViewHelper.php
@@ -1,29 +1,42 @@
 <?php
 namespace SKYFILLERS\SfSimpleFaq\ViewHelpers;
 
+	/*                                                                        *
+	 * This script is backported from the TYPO3 Flow package "TYPO3.Fluid".   *
+	 *                                                                        *
+	 * It is free software; you can redistribute it and/or modify it under    *
+	 * the terms of the GNU Lesser General Public License, either version 3   *
+	 *  of the License, or (at your option) any later version.                *
+	 *                                                                        *
+	 * The TYPO3 project - inspiring people to share!                         *
+	 *                                                                        */
 
 /**
  * Class CategoryActiveViewHelper
+ *
+ * @author Alexander Schnoor
  *
  * @package SKYFILLERS\SfSimpleFaq\ViewHelpers
  */
 class CategoryActiveViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
 
 
-    /**
-     *displays the select-status of a category
-     *
-     * @param int $categoryUid
-     * @param string $categoryString
-     * @return string
-     */
-    public function render($categoryUid ,$categoryString) {
+	/**
+	 * Displays the select-status of a category
+	 *
+	 * @param int $currentCategory The Category to check if it's selected
+	 * @param string $selectedCategories The
+	 *
+	 * @return string
+	 */
+	public function render($currentCategory, $selectedCategories) {
 
-        if(strpos($categoryString, (string)$categoryUid) !== false) {
-            return 'ausgewählt';
-        }else {
-            return 'nicht ausgewählt';
-        }
-    }
+		$selected = explode(',', $selectedCategories);
+		if (in_array($currentCategory, $selected) == TRUE) {
+			return 'ausgewählt';
+		} else {
+			return 'nicht ausgewählt';
+		}
+	}
 
 }
