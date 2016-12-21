2869c82656a5474340b925d235be5d35f109c145
schreibersebastian.de <breakpoint@schreibersebastian.de>
2016-12-21 12:21:47 +0100

    Add tt_content.php to include static TypoScript and Flexforms and Plugin in there

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,22 @@
+<?php
+
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
+}
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'sf_simple_faq',
+    'Pifaq',
+    'Simple FAQ'
+);
+
+/* Add Flexform */
+$extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase('sf_simple_faq');
+$pluginSignature = strtolower($extensionName) . '_pifaq';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:sf_simple_faq/Configuration/FlexForms/Flexform_plugin.xml');
+
+/* Static TypoScript */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('sf_simple_faq', 'Configuration/TypoScript', 'Simple FAQ');
+
+
